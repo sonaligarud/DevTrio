dcd2ca25762cdca1466b96af5969c8daa4f02f0c
sonali garud <sonali.issc@gmail.com>
2026-07-22 01:46:19 +0530

    ui changes

---
+++ src/DownloadResume.js
@@ -0,0 +1,57 @@
+import { Box, Button } from "@mui/material";
+import CustomTooltip from "./CustomTooltip";
+
+const buttonStyles = {
+  px: "18px",
+  py: "12px",
+  borderRadius: "10px",
+  border: "1px solid transparent",
+  background: `linear-gradient(#1C1C1C, #3A3A3A) padding-box, linear-gradient(11deg, #00FF85 0%, #8C8C8C 14%, #8C8C8C 87%, #00FF85 100%) border-box;
+  `,
+  color: "#fff",
+  fontSize: "13px",
+  fontWeight: 500,
+  textTransform: "none",
+  minWidth: "fit-content",
+  flexShrink: 0,
+  boxShadow: "none",
+  "& .MuiButton-startIcon": {
+    marginRight: "10px",
+    marginLeft: 0,
+  },
+  "&:hover": {
+     background: `linear-gradient(#1C1C1C, #3A3A3A) padding-box, linear-gradient(11deg, #00FF85 0%, #8C8C8C 14%, #8C8C8C 87%, #00FF85 100%) border-box;
+  `,
+    boxShadow: "none",
+  },
+  "&:active": { boxShadow: "none" },
+};
+
+function DownloadResume({ sx }) {
+  return (
+    <CustomTooltip
+      title={<>You can download the resume by clicking here</>}
+      placement="top"
+    >
+      <Button
+        component="a"
+        href="/Round-1_Corrections.pdf"
+        download
+        variant="contained"
+        startIcon={
+          <Box
+            component="img"
+            src="/assets/icons/download-resume.svg"
+            alt=""
+            sx={{ width: 20, height: 20 }}
+          />
+        }
+        sx={{ ...buttonStyles, ...sx }}
+      >
+        Download Resume
+      </Button>
+    </CustomTooltip>
+  );
+}
+
+export default DownloadResume;
